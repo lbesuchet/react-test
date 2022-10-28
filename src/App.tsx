d43ab773aefcc49e2733dcb0d153c1b9b5f323e9
Luc Besuchet <luc.besuchet@kudelskisecurity.com>
2022-10-28 11:06:20 +0200

    usecallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
 import './App.css';
+import Todos from './Todos';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["nothing"]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = useCallback(() => {
+    setTodos((t: Array<string>) => [...t, "New Todo"]);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
   );
 }
 
---
+++ src/Todos.tsx
@@ -0,0 +1,21 @@
+import { memo } from "react";
+
+interface Props {
+  todos: Array<string>;
+  addTodo: () => void;
+}
+
+const Todos = ({ todos, addTodo } : Props) => {
+  console.log("child render");
+  return (
+    <>
+      <h2>My Todos</h2>
+  {todos.map((todo, index) => {
+    return <p key={index}>{todo}</p>;
+  })}
+  <button onClick={addTodo}>Add Todo</button>
+  </>
+);
+};
+
+export default memo(Todos);
